c894873819e24a1738a540f4267ff151c0584e0d
developer1110 <vid.naidu@gmail.com>
2020-04-23 13:42:13 -0400

    changed text

--- src/com/vn/cj/WelcomeToJava.java
+++ src/com/vn/cj/WelcomeToJava.java
@@ -1,7 +1,7 @@
 package com.vn.cj;
 
 public class WelcomeToJava {
-public static void main(String[] args) {
-	System.out.println("Welcome to Java");
-}
+	public static void main(String[] args) {
+		System.out.println("Welcome to First GIT Project");
+	}
 }
